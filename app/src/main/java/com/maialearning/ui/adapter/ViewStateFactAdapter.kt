fd45e863a66533d8ec7fb2b563bdbf90204ca5e2
suman <sumanpreetkour1994@gmail.com>
2022-06-19 22:25:30 +0530

    changes done

--- app/src/main/java/com/maialearning/ui/adapter/ViewStateFactAdapter.kt
+++ app/src/main/java/com/maialearning/ui/adapter/ViewStateFactAdapter.kt
@@ -17,16 +17,16 @@ class ViewStateFactAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycl
             return CommunityFragmet()
         } else if (position == 2) {
             return AdmissionFragment()
-        } else if (position == 5) {
+        } else if (position == 3) {
+            return CostFragment()
+        }else if (position == 4) {
+            return DegreesFragment()
+        }else if (position == 5) {
             return TransferFragment()
         }else if (position == 6) {
             return FactsNotesFragment()
         }else if (position == 7) {
             return CampusFragment()
-        }else if (position == 4) {
-            return DegreesFragment()
-        }else if (position == 3) {
-            return CostFragment()
         }
         return OverViewFragment()
     }
