9bc8ab424369d5a9a7eded13a14605a2e4787282
payalarora <arorapayal68@gmail.com>
2022-06-22 16:39:23 +0530

    portfolio and upcoming

---
+++ app/src/main/java/com/maialearning/ui/fragments/PortfolioProfileFragment.kt
@@ -0,0 +1,34 @@
+package com.maialearning.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.maialearning.databinding.PortfolioProfileFragmentBinding
+
+
+class PortfolioProfileFragment : Fragment() {
+    private lateinit var mBinding: PortfolioProfileFragmentBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        mBinding= PortfolioProfileFragmentBinding.inflate(inflater,container,false)
+        return mBinding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
